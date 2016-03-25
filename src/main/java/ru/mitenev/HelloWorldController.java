a4a895829561991b929a649214d946da46582712
amitenev <aomitenev@gmail.com>
2016-03-25 17:02:43 +0300

    16.1 executing jar

--- src/main/java/ru/mitenev/HelloWorldController.java
+++ src/main/java/ru/mitenev/HelloWorldController.java
@@ -5,8 +5,8 @@ import org.joda.time.LocalTime;
 //@Controller
 public class HelloWorldController {
     public static void main(String[] args) {
-
-//        LocalTime current = new
+        LocalTime current = new LocalTime();
+        System.out.println("The current time is: " + current);
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
